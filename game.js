3e361a0f3cdf2c283c048698282b6e3489abcce4
Jyk <jray7890@naver.com>
2023-05-27 19:56:22 +0900

    feat: 점수계산 함수 수정

--- game.js
+++ game.js
@@ -10,3 +10,21 @@ const endGame = () => {
   const customWindow = window.open('', '_blank', '');
   customWindow.close();
 }
+
+
+/* game ver.commit 1.js에서 score를 받아서 print하는 함수*/
+
+let score1=0;
+let score2=0;
+let score3=0;
+if(localStorage.getItem('score1')!=null){
+  score1=localStorage.getItem('score1');
+}
+if(localStorage.getItem('score2')!=null){
+  score2=localStorage.getItem('score2');
+}
+if(localStorage.getItem('score3')!=null){
+  score3=localStorage.getItem('score3');
+}
+let total=parseInt(score1)+parseInt(score2)+parseInt(score3);
+$("#score").text(total);
